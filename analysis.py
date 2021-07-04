f511e63e61d977deaa05ffe97ec85a6c0df6f1f4
Amadou Ndiaye <ahmad.ndiayee@gmail.com>
2021-07-04 23:26:14 +0000

    implement analysis functions

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
 import os
-from pyspark.sql.functions import col
+from pyspark.sql.functions import count, sum, col
 from pyspark.sql import SparkSession
 import configparser
 
@@ -19,16 +19,37 @@ spark = SparkSession.builder \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
     .getOrCreate()
 
-
 config.read(CONF_FILE)
 orders = config.get("paths", 'orders')
 order_items = config.get("paths", 'order_items')
 customers = config.get("paths", 'customers')
-
+output = config.get("paths", 'output')
 
 orders_df = spark.read.option("header", "true").option("inferSchema", "true").csv(orders)
 order_items_df = spark.read.option("header", "true").option("inferSchema", "true").csv(order_items)
 customers_df = spark.read.option("header", "true").option("inferSchema", "true").csv(customers)
-orders_df.show()
 
 
+# orders_df.printSchema()
+# order_items_df.printSchema()
+# customers_df.printSchema()
+
+def join_dataset():
+    df = customers_df \
+        .join(orders_df, customers_df.customer_id == orders_df.customer_id) \
+        .join(order_items_df, orders_df.order_id == order_items_df.order_id) \
+        .select(orders_df.customer_id, col("customer_city"), col("customer_state"), orders_df.order_id, col("price"))
+    return df
+
+
+def get_top_customers():
+    df = join_dataset() \
+        .groupBy(col("customer_id")) \
+        .agg(count("order_id").alias("number_of_orders"), sum("price").alias("total_price")) \
+        .orderBy(col("total_price"), ascending=False)
+
+    return df
+
+
+def write_result_to_s3(df, format_output):
+    df.write.format(format_output).save(output)
